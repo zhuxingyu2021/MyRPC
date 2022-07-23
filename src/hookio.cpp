8d4a75f6c8760f8ace3c28417f92dd5771f59e69
zhuxingyu2021 <zhuxy97@mail2.sysu.edu.cn>
2022-07-23 22:54:24 +0800

    hook socket io

--- src/hookio.cpp
+++ src/hookio.cpp
@@ -6,22 +6,35 @@
 #include <unistd.h>
 #include <dlfcn.h>
 #include <sys/epoll.h>
+#include <sys/socket.h>
 
 namespace MyRPC{
     // 原始的系统调用入口
+    // 文件系统调用
     ssize_t (*sys_read)(int fd, void *buf, size_t count) = nullptr;
     ssize_t (*sys_write)(int fd, const void *buf, size_t count) = nullptr;
     int (*sys_close)(int fd) = nullptr;
+
+    // socket调用
+    int (*sys_accept)(int sockfd, struct sockaddr *addr, socklen_t *addrlen) = nullptr;
+    int (*sys_connect)(int sockfd, const struct sockaddr *addr, socklen_t addrlen) = nullptr;
+    ssize_t (*sys_recv)(int sockfd, void *buf, size_t len, int flags) = nullptr;
+    ssize_t (*sys_send)(int sockfd, const void *buf, size_t len, int flags) = nullptr;
 
     namespace Initializer {
         int _hook_io_initializer = []() {
             sys_read = (ssize_t (*)(int, void *, size_t))dlsym(RTLD_NEXT, "read");
             sys_write = (ssize_t (*)(int, const void *, size_t))dlsym(RTLD_NEXT, "write");
             sys_close = (int (*)(int))dlsym(RTLD_NEXT, "close");
+
+            sys_accept = (int (*)(int, struct sockaddr *, socklen_t *))dlsym(RTLD_NEXT, "accept");
+            sys_connect = (int (*)(int, const struct sockaddr *, socklen_t))dlsym(RTLD_NEXT, "connect");
+            sys_send = (ssize_t (*)(int, const void *, size_t, int))dlsym(RTLD_NEXT, "send");
+            sys_recv = (ssize_t (*)(int, void *, size_t, int))dlsym(RTLD_NEXT, "recv");
             return 0;
         }();
     }
-}
+}// namespace MyRPC
 
 using namespace MyRPC;
 
@@ -93,4 +106,101 @@ ssize_t write(int fd, const void *buf, size_t count) {
 int close(int fd) {
     return sys_close(fd);
 }
+
+// 覆盖posix accept函数
+int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
+    if (enable_hook) {
+        enable_hook = false;
+
+#if MYRPC_DEBUG_LEVEL >= MYRPC_DEBUG_HOOK_LEVEL
+        Logger::debug("Thread:{} Fiber:{} trying to accept({}, {}, {})", FiberPool::GetCurrentThreadId(),
+                            MyRPC::Fiber::GetCurrentId(), sockfd, (void*)addr, *addrlen);
+#endif
+        auto err = FiberPool::GetEventManager()->AddIOEvent(sockfd, EventManager::READ);
+        if(!err){
+            Fiber::Block();
+        }
+        else {
+            switch (errno) {
+                default:
+                    MYRPC_SYS_ASSERT(false);
+            }
+        }
+    }
+    return sys_accept(sockfd, addr, addrlen);
 }
+
+// 覆盖posix connect函数
+int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    if (enable_hook) {
+        enable_hook = false;
+
+#if MYRPC_DEBUG_LEVEL >= MYRPC_DEBUG_HOOK_LEVEL
+    Logger::debug("Thread:{} Fiber:{} trying to connect({}, {}, {})", FiberPool::GetCurrentThreadId(),
+                                MyRPC::Fiber::GetCurrentId(), sockfd, (void*)addr, addrlen);
+#endif
+
+        auto err = FiberPool::GetEventManager()->AddIOEvent(sockfd, EventManager::WRITE);
+        if(!err){
+            Fiber::Block();
+        }
+        else {
+            switch (errno) {
+                default:
+                    MYRPC_SYS_ASSERT(false);
+            }
+        }
+    }
+    return sys_connect(sockfd, addr, addrlen);
+}
+
+// 覆盖posix recv函数
+ssize_t recv(int sockfd, void *buf, size_t len, int flags) {
+    if (enable_hook) {
+        enable_hook = false;
+
+#if MYRPC_DEBUG_LEVEL >= MYRPC_DEBUG_HOOK_LEVEL
+        Logger::debug("Thread:{} Fiber:{} trying to recv({}, {}, {}, {})", FiberPool::GetCurrentThreadId(),
+                            MyRPC::Fiber::GetCurrentId(), sockfd, buf, len, flags);
+#endif
+        auto err = FiberPool::GetEventManager()->AddIOEvent(sockfd, EventManager::READ);
+        if(!err){
+            Fiber::Block();
+        }
+        else {
+            switch (errno) {
+                default:
+                    MYRPC_SYS_ASSERT(false);
+            }
+        }
+    }
+    return sys_recv(sockfd, buf, len, flags);
+
+}
+
+// 覆盖posix send函数
+ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
+    if (enable_hook) {
+        enable_hook = false;
+
+#if MYRPC_DEBUG_LEVEL >= MYRPC_DEBUG_HOOK_LEVEL
+        Logger::debug("Thread:{} Fiber:{} trying to send({}, {}, {}, {})", FiberPool::GetCurrentThreadId(),
+                            MyRPC::Fiber::GetCurrentId(), sockfd, buf, len, flags);
+#endif
+
+        auto err = FiberPool::GetEventManager()->AddIOEvent(sockfd, EventManager::WRITE);
+        if(!err){
+            Fiber::Block();
+        }
+        else {
+            switch (errno) {
+                default:
+                    MYRPC_SYS_ASSERT(false);
+            }
+        }
+    }
+    return sys_send(sockfd, buf, len, flags);
+}
+
+
+}// extern "C"
---
+++ test/moduletest/testhooksocketio.cpp
@@ -0,0 +1,100 @@
+#include "fiberpool.h"
+#include "logger.h"
+#include <fcntl.h>
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+using namespace MyRPC;
+
+#define NUM_THREADS 1
+
+
+int main()
+{
+    FiberPool fp(NUM_THREADS);
+
+    int port_server = 9999;
+    char ip[16] = "127.0.0.1";
+
+    fp.Start();
+
+    auto client = fp.Run([&ip,port_server](){
+        char s[1024];
+        memset(s, 0, sizeof(s));
+
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if(sockfd < 0){
+            Logger::error("socket error");
+            return;
+        }
+
+        struct sockaddr_in remote_addr;
+        remote_addr.sin_family = AF_INET;
+        remote_addr.sin_port = htons(port_server);
+        remote_addr.sin_addr.s_addr = inet_addr(ip);
+        if(connect(sockfd, (struct sockaddr*)&remote_addr, sizeof(remote_addr)) < 0){
+            Logger::error("connect error");
+            return;
+        }
+        Logger::info("connect success");
+        char buf[1024] = {0};
+        while(true){
+            int read_cnt = 0;
+            int input_size = read(STDIN_FILENO, s, sizeof(s));
+            while(input_size> 0){
+                if(s[read_cnt]=='\n') break;
+                read_cnt++;
+            }
+            s[++read_cnt] = '\0';
+            send(sockfd, s, read_cnt, 0);
+        }
+    }, 0);
+
+    auto server = fp.Run([&ip,port_server](){
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if(sockfd < 0){
+            Logger::error("socket error");
+            return;
+        }
+        struct sockaddr_in local_addr;
+        local_addr.sin_family = AF_INET;
+        local_addr.sin_port = htons(port_server);
+        local_addr.sin_addr.s_addr = inet_addr(ip);
+        if(bind(sockfd, (struct sockaddr*)&local_addr, sizeof(local_addr)) < 0){
+            Logger::error("bind error");
+            return;
+        }
+        if(listen(sockfd, 5) < 0){
+            Logger::error("listen error");
+            return;
+        }
+        struct sockaddr_in remote_addr;
+        socklen_t len = sizeof(remote_addr);
+        int new_sockfd = accept(sockfd, (struct sockaddr*)&remote_addr, &len);
+        if(new_sockfd < 0){
+            Logger::error("accept error");
+            return;
+        }
+        Logger::info("accept success");
+        char buf[1024] = {0};
+        while(true){
+            int read_cnt = 0;
+            int input_size = recv(new_sockfd, buf, sizeof(buf), 0);
+            while(input_size> 0){
+                if(buf[read_cnt]=='\n') break;
+                read_cnt++;
+            }
+            buf[++read_cnt] = '\0';
+            write(STDOUT_FILENO, buf, read_cnt);
+        }
+    }, 0);
+
+    client.Join();
+    server.Join();
+
+    fp.Stop();
+}
+
